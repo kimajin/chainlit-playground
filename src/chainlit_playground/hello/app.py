97d25d0cd10b3e271cc00ed3d47a9255d3be6d9a
Satoshi Nakamura <yfa14710@nifty.com>
2026-03-15 13:58:09 +0900

    fix

--- src/chainlit_playground/hello/app.py
+++ src/chainlit_playground/hello/app.py
@@ -1,6 +1,16 @@
 import chainlit as cl
 
 
-@cl.on_chat_start
-async def main() -> None:
-    await cl.Message(content="Hello World").send()
+@cl.on_message
+async def main(message: cl.Message) -> None:
+    await cl.Message(content=f"Received: {message.content}").send()
+
+
+@cl.set_starters
+async def set_starters(_user: cl.User | None) -> list[cl.Starter]:
+    return [
+        cl.Starter(
+            label="Message",  # 表示されるラベル
+            message="Hello World!",  # 送信されるメッセージ
+        )
+    ]
